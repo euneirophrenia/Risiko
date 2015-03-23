3870ac67374ca7c2d66bd1976baba54a221941c2
BeCarefulInTheJungle <marza.guitar@hotmail.it>
2015-03-23 21:42:04 +0100

    Scripts

---
+++ Scripts/Clicked.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clicked : MonoBehaviour 
+{
+	void OnMouseDown () 
+	{
+		GameObject co = this.gameObject.transform.parent.gameObject;
+		string parent = co.name;
+
+		Debug.Log ("Mi chiamo "+ this.gameObject.name + " e sono nel continente " 
+		           + parent );
+	}
+}
---
+++ Scripts/Welcome.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Welcome : MonoBehaviour
+{
+	public float timeToLive=5f;
+	public bool finalState=false;
+	// Use this for initialization
+	void Start ()
+	{
+		InvokeRepeating ("Activate", .5f, .5f);
+		
+	}
+	
+	void Activate()
+	{
+		if (this.gameObject.activeSelf)
+			this.gameObject.SetActive (false);
+		else
+			this.gameObject.SetActive (true);
+
+		if (Time.time > timeToLive)
+		{
+			CancelInvoke("Activate");
+			this.gameObject.SetActive(finalState);
+		}
+	}
+	
+	
+}
