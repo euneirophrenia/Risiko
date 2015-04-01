928cc50709abc7fc160ec7d0ee3c27ce5d4d8c2b
euneirophrenia <marcodiv@marcomac.station>
2015-04-01 19:39:53 +0200

    Script per la camera (vedi dettagli per istruzioni)
    -zoom (avanti e indietro): rotella del mouse;
    - frecce direzionali per spostare (potrebbe funzionare anche con wasd,
    ma non ho provato espressamente)
    -spazio per ripristinare la vista originaria, chiamabile anche da
    funzione, dall’esterno (così che ogni giocatore possa fare i suoi
    lavori e che poi la vista possa essere automaticamente ripristinata al
    default, cioè a quella della creazione, dal controller)

---
+++ Scripts/Follow.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour 
+{
+	private Vector3 original;
+	private Vector3 delta;
+	// Use this for initialization
+	void Start () 
+	{
+		original=this.gameObject.GetComponent<Transform>().position;
+		delta=new Vector3(0,2,0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetAxis("Mouse ScrollWheel") > 0)
+		{
+			this.gameObject.GetComponent<Transform>().position-=delta;
+		}
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0)
+		{
+			this.gameObject.GetComponent<Transform>().position+=delta;
+		}
+		if (Input.GetKey("up"))
+		{
+			this.gameObject.GetComponent<Transform>().position+=Vector3.forward;
+		}
+		if (Input.GetKey("down"))
+		{
+			this.gameObject.GetComponent<Transform>().position+=Vector3.back;
+		}
+		if (Input.GetKey("left"))
+		{
+			this.gameObject.GetComponent<Transform>().position+=Vector3.left;
+		}
+		if (Input.GetKey("right"))
+		{
+			this.gameObject.GetComponent<Transform>().position+=Vector3.right;
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			this.gameObject.GetComponent<Transform>().position=this.original;
+		}
+	
+	}
+
+}
