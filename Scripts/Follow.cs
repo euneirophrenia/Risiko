86b9987a57a8e5e053f32182844e052944d75039
euneirophrenia <marcodiv@marcomac.station>
2015-04-02 09:42:21 +0200

    Camera Script Improvements
    Ora è molto più configurabile.
    La camera si può muovere con WASD e frecce a default, ma risponde ai
    comandi impostati nell’input manager di unity, quindi volendo con
    qualsiasi cosa.
    Si può anche cambiare la velocità di spostamento/zoom per meglio
    adattarla alle varie esigenze..

--- Scripts/Follow.cs
+++ Scripts/Follow.cs
@@ -5,11 +5,14 @@ public class Follow : MonoBehaviour
 {
 	private Vector3 original;
 	private Vector3 delta;
+
+	public float zoomSpeed = 2.5f;
+	public float moveSpeedAmplifier=1;
 	// Use this for initialization
 	void Start () 
 	{
 		original=this.gameObject.GetComponent<Transform>().position;
-		delta=new Vector3(0,2,0.5f);
+		delta=new Vector3(0,zoomSpeed,0.5f);
 	}
 	
 	// Update is called once per frame
@@ -23,26 +26,27 @@ public class Follow : MonoBehaviour
 		{
 			this.gameObject.GetComponent<Transform>().position+=delta;
 		}
-		if (Input.GetKey("up"))
+		if (Input.GetAxis("Vertical")>0)
 		{
-			this.gameObject.GetComponent<Transform>().position-=Vector3.forward;
+			this.gameObject.GetComponent<Transform>().position-=moveSpeedAmplifier*Vector3.forward;
 		}
-		if (Input.GetKey("down"))
+		if (Input.GetAxis ("Vertical") < 0)
 		{
-			this.gameObject.GetComponent<Transform>().position-=Vector3.back;
+			this.gameObject.GetComponent<Transform>().position-=moveSpeedAmplifier*Vector3.back;
 		}
-		if (Input.GetKey("left"))
+		if (Input.GetAxis ("Horizontal")<0)
 		{
-			this.gameObject.GetComponent<Transform>().position-=Vector3.left;
+			this.gameObject.GetComponent<Transform>().position-=moveSpeedAmplifier*Vector3.left;
 		}
-		if (Input.GetKey("right"))
+		if (Input.GetAxis("Horizontal")>0)
 		{
-			this.gameObject.GetComponent<Transform>().position-=Vector3.right;
+			this.gameObject.GetComponent<Transform>().position-=moveSpeedAmplifier*Vector3.right;
 		}
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			this.Reset();	
+			this.Reset();
 		}
+	
 	}
 
 	public void Reset()
