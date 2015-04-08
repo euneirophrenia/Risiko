e436b5fe9ee3472750fbdfbacf815cf65aaddeac
BeCarefulInTheJungle <marza.guitar@hotmail.it>
2015-04-08 19:08:37 +0200

    Aggiornamento Manager.cs
    Aggiornamento script per DiceManager

--- Scripts/Manager.cs
+++ Scripts/Manager.cs
@@ -3,19 +3,54 @@ using System.Collections;
 
 public class Manager : MonoBehaviour 
 {
+    private bool finito;
+    private bool app;
     public Transform attack;
     public Transform defense;
+    private Transform [] attackList;
+    private Transform [] defenseList;
+    //private int [] attackResult;
+    //private int [] defenseResult;
 
 	// Use this for initialization
 	void Start ()
     {
-        Instantiate(attack);
-        Instantiate(defense);
+        finito = true;
+        app = false;
+
+        this.attackList = new Transform[3];
+        this.defenseList = new Transform[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            this.attackList[i] = Instantiate(attack);
+            this.defenseList[i] = Instantiate(defense);
+            
+        }
+
+        
+ 
 	}
 	
-	// Update is called once per frame
-	void Update () 
+	void Update()
     {
-	
-	}
+        finito = true;
+
+        for (int i = 0; i < 3; i++)
+        {
+            finito &= this.attackList[i].gameObject.GetComponent<DiceRoll>().Done;
+            finito &= this.defenseList[i].gameObject.GetComponent<DiceRoll>().Done;
+        }
+
+        if (finito && !app)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Debug.Log(this.attackList[i].gameObject.GetComponent<DiceRoll>().Value);
+                Debug.Log(this.defenseList[i].gameObject.GetComponent<DiceRoll>().Value);
+            }
+            app = true;
+        }
+    }
+    
 }
