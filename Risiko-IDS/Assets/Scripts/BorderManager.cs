0bfd61dedd3982026cfaa9d876733bd1b70372af
euneirophrenia <marcodiv@marcomac.station>
2015-05-10 10:56:34 +0200

    Gestore dei confini (e immagine mappa riferimento)
    Se non ricordo male, il png che ho messo nella cartella immagini è la
    mappa da cui scanc è partito nel fare il model 3D. Allora ho ripreso i
    confini “oceanici” che erano segnati lì e scritto quelli.
    
    Lo script funziona attaccato ad un gameobject e con il parametro
    stringa pari al path del file .txt (se non lo spostate da dove l’ho
    messo, dovrebbe andare già così, se no cambiatelo).
    Il file txt contiene materialmente l’elenco dei confini. Se qualche
    confine non va, ho sbagliato io col copia-incolla, modificatelo
    liberamente..
    Volendo, non è indispensabile attaccare lo script ad un gameobject, in
    linea di principio potremmo anche definirlo come tipo a sé stante, però
    sarebbe bene che fosse singleton, e quindi occorrerebbe anche una
    factory a supporto. Per questo motivo, ritengo più easy assegnarlo a un
    solo gameobject e chi vuole chiede tutto a lui.
    
    Lo script usa linq e una lambda ^^ giusto per prendere i bonus!

---
+++ Risiko-IDS/Assets/Scripts/BorderManager.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+
+public class BorderManager : MonoBehaviour 
+{
+	//necessaria perché unity ha una storia di non supporto alle tuple, niente di speciale in realtà.
+	#region Confine 
+	private struct Confine 
+	{
+		private readonly string[] _stati;
+
+		public Confine(string stato1, string stato2)
+		{
+			_stati=new string[2];
+			_stati[0]=stato1;
+			_stati[1]=stato2;
+		}
+
+		public string this[int index]
+		{
+			get
+			{
+				if (index<0 || index>=2)
+					throw new UnityException("no such element");
+				else
+					return _stati[index];
+			}
+		}
+
+		public static bool operator == (Confine a, Confine b) //mi fa comodo una cifra! vedi linq sotto
+		{
+			return (a[0].Equals(b[0]) && a[1].Equals(b[1])) || (a[1].Equals(b[0]) && a[0].Equals(b[1]));
+		}
+		public static bool operator != (Confine a, Confine b) //unity dava warning senza questo (ci sta)
+		{
+			return ! (a==b);
+		}
+
+		public override int GetHashCode() //unity dava warning senza questo (poteva farsene una ragione eh)
+		{
+			return this[0].GetHashCode() ^ this[1].GetHashCode();
+		}
+
+		public override bool Equals (object obj) //unity dava warning senza questo (ma in questo caso condivido)
+		{
+			if (obj is Confine)
+				return this == (Confine)obj;
+			return false;
+		}
+
+	}
+	#endregion
+
+
+	private static readonly List<Confine> _borders = new List<Confine>();
+	public string configurationFilePath="./Assets/Scripts/confini.txt";
+
+	void Start () 
+	{
+		StreamReader sr = new StreamReader(configurationFilePath);
+		string line;
+		while ((line=sr.ReadLine())!=null)
+		{
+			string[] tokens = line.Split(',').Select(s=> s.Trim()).ToArray();
+			_borders.Add(new Confine(tokens[0], tokens[1]));
+		}
+		sr.Close ();
+
+		//Debug.Log (areNeighbours("North_America_03", "South_America_04"));
+	}
+
+	public static bool areNeighbours (string a, string b) 
+	{
+		if (a.Equals(b)) // che fare se chiedono se uno stato confina con lui stesso? 
+			return true;
+
+		Confine x = new Confine(a,b);
+		IEnumerable<Confine> res=	from c in _borders
+									where c==x
+		 							select c;
+		return res.Count()>0;
+	}
+
+
+
+	public static bool areNeighbours(GameObject a, GameObject b)
+	{
+		string s1=a.name;
+		string s2=b.name;
+
+		return areNeighbours(s1,s2);
+	}
+
+	public static bool areNeighbours(StatoController c1, StatoController c2)
+	{
+		string s1 = c1.gameObject.name;
+		string s2= c2.gameObject.name;
+
+		return areNeighbours(s1, s2);
+	}
+
+}
