14e244cf0c82274e73a3b14f4e602cc8d540c3e1
euneirophrenia <marcodiv@marcomac.station>
2015-05-19 20:34:48 +0200

    Il Main Manager
    A parte qualche rifinitura e qualche utilità che può servire
    ulteriormente, il codice sarebbe già a posto, nelle sue funzioni
    principali.
    Enjoy the mindfuck.

--- Risiko-IDS/Assets/Scripts/MainManager.cs
+++ Risiko-IDS/Assets/Scripts/MainManager.cs
@@ -1,30 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System;
 
 public class MainManager : MonoBehaviour 
 {
-    public List<StatoController> states;
+	private readonly Dictionary<string, List<StatoController>> _world = new Dictionary<string, List<StatoController>>();
+	private static readonly Dictionary<string, IManager> _factory=new Dictionary<string,IManager>();
+	public GameObject world;
 
-    private List<Giocatore> players;
+    private IEnumerable<Giocatore> players;
     private static MainManager instance;            //DA TESTARE
 
 	// Use this for initialization
 	void Start () 
     {
         this.players = new List<Giocatore>();
-        instance = this;                            //DA TESTARE
+        instance = this; 							//DA TESTARE
+
+		foreach(Transform continent in this.world.transform)
+		{
+			_world[continent.gameObject.name]=new List<StatoController>();
+			foreach(Transform stato in continent)
+			{
+				_world[continent.gameObject.name].Add(stato.GetComponent<StatoController>());
+			}
+		}
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 
     public static IManager GetManagerInstance(string manager)
     {
-        return null;
-    }
+        if (_factory.ContainsKey(manager))
+			return _factory[manager];
+
+		Type managerType = Type.GetType(manager);
+
+		if (! (managerType is IManager))
+			throw new ArgumentException("Manager ignoto");
+
+		_factory[manager]= (IManager) Activator.CreateInstance(managerType); 
+		//buono se ogni manager espone il costruttore di default
+		/* una cosa molto tranquilla che potremmo fare è: ogni manager ha il costruttore di default e se ha bisogno di altre info le chiede 
+		 * a qualche altro manager, come d'altronde abbiamo sempre fatto fin'ora, si può continuare così e questo codice funziona.
+		 Alternativa -> il mainmanager si tiene una mappa manager-argomenti opportuni, e poi si usano quelli. Possiamo scegliere*/
+
+		return _factory[manager];
+	}
 
     public static MainManager GetInstance()
     {
@@ -34,16 +58,28 @@ public class MainManager : MonoBehaviour
     public void Init(string[] playerNames)
     {
         InitialPhaseManager init = new InitialPhaseManager();
-        this.players = init.Create(playerNames, this.states); 
+        this.players = init.Create(playerNames, new List<StatoController>(this.States)); 
     }
 
-    public List<StatoController> States
+    public IEnumerable<StatoController> States
     {
-        get
-        {
-            return this.states;
+		get
+		{
+			return from StatoController s in (this._world.Values.SelectMany(x => x).ToList()) select s;
         }
     }
+
+	public IEnumerable<StatoController> GetStatesByPlayer(Giocatore g)
+	{
+		return from StatoController c in States where c.Player.Equals(g) select c;
+	}
+
+	public IEnumerable<StatoController> GetStatesyByContinent(string name)
+	{
+		if (!_world.ContainsKey(name))
+			throw new KeyNotFoundException("No such continent");
+		return _world[name];
+	}
 
 
 }
