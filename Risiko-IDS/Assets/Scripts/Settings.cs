14ec7875c23f20b8f37a59294f9bb44222a37cd3
BeCarefulInTheJungle <marza.guitar@hotmail.it>
2015-06-01 17:29:11 +0200

    Aggiornato TODO.txt e Settings
    Inserito in Settings il dizionario per permettere l'assegnazione di n
    armate qualore un continente sia in possesso di un giocatore

--- Risiko-IDS/Assets/Scripts/Settings.cs
+++ Risiko-IDS/Assets/Scripts/Settings.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 public static class Settings
 {
@@ -16,6 +17,15 @@ public static class Settings
 	private static readonly int[] _players= {2,3,6};
 	private static readonly Color[] _colors = { Color.red, Color.blue, Color.cyan, Color.green, Color.yellow, Color.black};
 	private static readonly int _minStati=24, _maxStati=27;
+    private static readonly Dictionary<string, int> _armatePerContinente = new Dictionary<string, int>()
+    {
+        {"North_America", 4},
+        {"South_America", 3},
+        {"Asia", 7},
+        {"Africa", 5},
+        {"Oceania", 2},
+        {"Europa", 4}
+    };
 
 	public static string BorderFile
 	{
@@ -72,5 +82,13 @@ public static class Settings
 			return _maxStati;
 		}
 	}
+
+    public static Dictionary<string, int> ArmatePerContinente
+    {
+        get
+        {
+            return _armatePerContinente;
+        }
+    }
 	
 }
